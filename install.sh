2b0581fec629f42967c591161e12971e239ed7a1
Jony Rivera <jrsepulveda1988@gmail.com>
2026-01-15 20:09:28 -0300

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/dzh0ni/Webshell_Scanner.git
-chmod +x Webshell_Scanner/*
-cd Webshell_Scanner
-ls -ltha
-sudo python3 -m pip install -r requirements.txt 
-sudo apt installl -y dirb 
